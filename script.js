4afe74df95758acfe72c8d357c889c9170c445c5
Зелёный Андрей Сергеевич <andrey.zelenyy@south.rt.ru>
2023-09-27 13:52:03 +0300

    Init

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+    // canvas setup
+    const canvas = this.document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;    
+})
